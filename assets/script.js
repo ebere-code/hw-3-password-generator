531c30f5b9ea2ef12e4568d1efaaaa01ded97f67
ebere-code <ebereezeobiuni@gmail.com>
2022-04-26 19:42:36 -0400

    completed generate password function

--- assets/script.js
+++ assets/script.js
@@ -10,31 +10,6 @@ function writePassword() {
 }
 
 
-// //assign global variable
-// var length = document.getElementById('numCharacters');
-// var lowercase = document.getElementById('incLowercase');
-// var uppercase = document.getElementById('incUppercase');
-// var numbers = document.getElementById('incNumbers');
-// var symbols = document.getElementById('incSymbols');
-// var form = document.getElementById('passwordForm');
-// var passwordText = document.getElementById('password'); 
-
-// var password = document.getElementById('password');
-
-// const randomFunction = {
-//     lower: getRandomLower,
-//     upper: getRandomUpper,
-//     number: getRandomNumber,
-//     symbol: getRandomSymbol
-// };
-
-// Add event listener to generate button 
-// generateBtn.addEventListener("click", () => {
-
-// var numCharacters = +length.value;
-// var incUppercase = uppercase.checked;
-// var incLowercase = lowercase.checked;
-// var incNumbers = numbers.checked;
 // var incSymbols = symbols.checked;
 
 function generatePassword() {
@@ -42,14 +17,14 @@ function generatePassword() {
     var characters = {
       lowercase: "abcdefghijklmnopqrstuvwxyz",
       uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-      /* need to add quotes */
+      // need to add quotes 
       symbols: ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"],
       numbers: "0123456789"
     }
     var correctFormat = false;
 
     while (!correctFormat) {
-        /* Checks to make sure the password is the correct length */
+        // Checks to make sure the password is the correct length 
         var passwordLength = window.prompt("Please enter the length of your password: ");
         if (Math.floor(passwordLength) < 8) {
             window.alert("Password must be equal to or longer than 8 characters. Please try again.");
@@ -62,19 +37,113 @@ function generatePassword() {
             var includeUppercase = window.confirm ('Do you want password to include upper letter?');
             var includeNumber = window.confirm ('Do you want password to include numbers?');
             var includeSymbol = window.confirm ('Do you want password to include symbols?');
+
+            if (!(includeLowercase || includeUppercase || includeNumber || includeSymbol)) {
+                window.alert("Please choose at least one criteria for your password and try again.");
+            } else {
+                //starts building password string 
+                if (includeNumber) {
+                    passwordValue += characters.numbers;
+                }
+                if (includeLowercase) {
+                    passwordValue += characters.lowercase;
+                }
+                if (includeUppercase) {
+                    passwordValue += characters.uppercase;
+                }
+                if (includeSymbol) {
+                    passwordValue += characters.symbols;
+                    // Generates the random password 
+                }
+                var password = generateString(passwordValue, passwordLength);
+                var allPresent = {includeNumber, includeLowercase, includeUppercase, includeSymbol};
+            
+                // Checks to make sure that all user requirements are present in the randomized password 
+                while (!checkIfAllPresent(allPresent, password, characters)) {
+                    password = generateString(passwordValue, passwordLength);
+                }
+                
+                return password;
+                
+            }
         }
     }
 }
 
+function checkIfAllPresent(allPresent, password, characters) {
+    var afterCheckPresent = true;
+    var checkArray = {includeNumber: false, includeLowercase: false, includeUppercase: false, includeSymbol: false};
+  
+    // Check if numbers are present in the password
+    if (allPresent.includeNumber) {
+      for (var i=0; i<password.length; i++) {
+        if (characters.numbers.includes(password[i])) {
+          checkArray.includeNumber = true;
+        }
+      }
+    }
+  
+    // Check if lowercase are present in the password
+    if (allPresent.includeLowercase) {
+      for (var i=0; i<password.length; i++) {
+        if (characters.lowercase.includes(password[i])) {
+          checkArray.includeLowercase = true;
+        }
+      }
+    }
+  
+    // Check if uppercase are present in the password
+    if (allPresent.includeUppercase) {
+      for (var i=0; i<password.length; i++) {
+        if (characters.uppercase.includes(password[i])) {
+          checkArray.includeUppercase = true;
+        }
+      }
+    }
+  
+    // Check if uppercase are present in the password
+    if (allPresent.includeSymbol) {
+      for (var i=0; i<password.length; i++) {
+        if (characters.symbols.includes(password[i])) {
+          checkArray.includeSymbol = true;
+        }
+      }
+    }
+      
+    // This checks the two boolean maps
+    var i=0;
+    while (afterCheckPresent && i<Object.values(allPresent).length) {
+      if (Object.values(allPresent)[i] !== Object.values(checkArray)[i]) {
+        afterCheckPresent = false;
+      }
+      i++;
+    }
+  
+    return afterCheckPresent;
+  }
+  
+  // Generates the password string using random integers
+  function generateString(criteria, passwordLength) {
+    var password="";
+    for (var i=0; i<passwordLength; i++) {
+      var randomInt = Math.floor(Math.random() * criteria.length);
+      password += criteria[randomInt];
+    }
+    return password;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
 
 
 //Set get password character from user
-if(numCharacters <8 || numCharacters >128) {
-    return alert("Please enter a number from 8-128 characters.")
-  } else {
-    passwordText.innerText = generatePassword(numCharacters, incLowercase, incUppercase, incNumbers, incSymbols);}
+// if(numCharacters <8 || numCharacters >128) {
+//     return alert("Please enter a number from 8-128 characters.")
+//   } else {
+//     passwordText.innerText = generatePassword(numCharacters, incLowercase, incUppercase, incNumbers, incSymbols);}
   
-  });
+//   });
 
 
 
@@ -104,9 +173,6 @@ if(numCharacters <8 || numCharacters >128) {
 //     return text;
 // };
 
-var passwordLength = window.prompt ('How many characters long would you like your password to be?');
-var expression1 = (passwordLength < 8);
-var expression2 = (passwordLength > 128);
 
 // console.log (password.length);
 
@@ -116,12 +182,7 @@ var expression2 = (passwordLength > 128);
 // includeSymbol = false;
 
 // if (passwordLength = null) {
-//     return;
-// }
-if (expression1 || expression2) 
-{
-    passwordLength = window.prompt ('Please Choose a character count between 8 and 128');
-};
+
 
 
 var passphrase = '';
